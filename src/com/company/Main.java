2b5779376b83eb2bfe3e061e218ba86ee4c30c6c
Bartłomiej Bugała <bartomiej.bugala@gmail.com>
2020-11-14 17:48:01 +0100

    Added short description of application

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// This is a simple Java Basic application to management list of cars in the showroom
     }
 }
