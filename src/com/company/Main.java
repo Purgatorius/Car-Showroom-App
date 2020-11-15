b8aad11e0dcde5a30c31dddf05e71a6a4a27f537
Bartłomiej Bugała <bartomiej.bugala@gmail.com>
2020-11-15 14:31:59 +0100

    created simple switch loop for userChoice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
 	// This is a simple Java Basic application to management list of cars in the showroom
         Scanner scanner = new Scanner(System.in);
+        boolean shouldContinue = true;
 
-        while(true) {
+        while(shouldContinue) {
             System.out.println(" ");
             System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("---------------------------------------------------------------------------------------");
@@ -24,7 +25,13 @@ public class Main {
             System.out.println("Choose from 1-5:");
             int userChoice = scanner.nextInt();
 
-
+            switch (userChoice) {
+                case 1 -> System.out.println("user chose 1 ");
+                case 2 -> System.out.println("user chose 2 ");
+                case 3 -> System.out.println("user chose 3 ");
+                case 4 -> System.out.println("user chose 4 ");
+                case 5 -> shouldContinue = false;
+            }
         }
     }
 }
