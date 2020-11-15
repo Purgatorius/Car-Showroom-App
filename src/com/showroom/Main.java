7d69f5eb05ceb443d17c97d01b78de406f9b3330
Bartlomiej Bugala <bartlomiej.bugala@gmail.com>
2020-11-15 18:27:46 +0100

    Created new Java class named Showroom, created 2 methods in showroom class

--- src/com/showroom/Main.java
+++ src/com/showroom/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.showroom;
 
 import java.util.Scanner;
 
--- src/com/showroom/Showroom.java
+++ src/com/showroom/Showroom.java
@@ -1,2 +1,16 @@
-package com.showroom;public class Showroom {
+package com.showroom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Showroom {
+    private List<String> cars = new ArrayList<>();
+
+    public void addCar(String car) {
+        cars.add(car);
+    }
+
+    public List<String> getCars() {
+        return cars;
+    }
 }
